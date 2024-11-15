5fabeb9ab0c025f4eaf7d5a02d30b184eedd4ce3
David Victor <davidvictorcontato7@gmail.com>
2024-11-14 23:04:08 -0300

    Update ContratacaoController.java

--- src/main/java/com/delas/api/controller/ContratacaoController.java
+++ src/main/java/com/delas/api/controller/ContratacaoController.java
@@ -1,13 +1,49 @@
 package com.delas.api.controller;
 
+import com.delas.api.model.ContratacaoModel;
+import com.delas.api.repository.ContratacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("contratacao")
+@RequestMapping("/contratacao")
 public class ContratacaoController {
 
     @Autowired
-    private ContratacaoController contratacaoController;
+    private ContratacaoRepository contratacaoRepository;
+
+    @PostMapping
+    public ContratacaoModel createContratacao(@RequestBody ContratacaoModel contratacao) {
+        return contratacaoRepository.save(contratacao);
+    }
+
+    @GetMapping
+    public List<ContratacaoModel> getAllContratacoes() {
+        return contratacaoRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ContratacaoModel getContratacaoById(@PathVariable Long id) {
+        return contratacaoRepository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/{id}")
+    public ContratacaoModel updateContratacao(@PathVariable Long id, @RequestBody ContratacaoModel contratacaoDetails) {
+        ContratacaoModel contratacao = contratacaoRepository.findById(id).orElse(null);
+        if (contratacao != null) {
+            contratacao.setCampoExemplo(contratacaoDetails.getCampoExemplo()); 
+            return contratacaoRepository.save(contratacao);
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteContratacao(@PathVariable Long id) {
+        contratacaoRepository.deleteById(id);
+    }
 }
+
+
+//
