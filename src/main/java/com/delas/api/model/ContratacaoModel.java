a941972b4fac000c640081bc45e532244615be8a
Bárbara Galdino <122044386+babil0nia@users.noreply.github.com>
2024-12-04 15:12:47 -0300

    Update ContratacaoModel.java

--- src/main/java/com/delas/api/model/ContratacaoModel.java
+++ src/main/java/com/delas/api/model/ContratacaoModel.java
@@ -1,8 +1,6 @@
 package com.delas.api.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,31 +17,25 @@ public class ContratacaoModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;  // Altere para "id", assim o método getId() será gerado automaticamente pelo Lombok
+    private Long idcontratacao;
 
-    // Relacionamento com o usuário - mudado o nome da propriedade
     @ManyToOne
     @JoinColumn(name = "usuario_id", nullable = false)
-    private UsuarioModel usuario;
+    private UsuarioModel id;
 
     @ManyToOne
     @JoinColumn(name = "idservicos", nullable = false)
-    private ServicosModel servicos;
+    private ServicosModel idservicos;
+
 
     @Column(name = "status", length = 20)
-    @NotNull
-    @Size(min = 1, max = 20)
     private String status;
+
 
     @Column(name = "datacriacao", nullable = false)
     private LocalDateTime dataContratacao = LocalDateTime.now();
 
-    @Column(name = "comentarios", nullable = false)
-    @NotNull
-    @Size(min = 1, message = "Comentários não podem ser vazios.")
-    private String comentarios;
 
-    // Relacionamento com a tabela Avaliação
-    @OneToOne(mappedBy = "contratacao", cascade = CascadeType.PERSIST)
-    private AvaliacaoModel avaliacao;
+    @Column(name = "comentarios", nullable = false)
+    private String comentarios;
 }
