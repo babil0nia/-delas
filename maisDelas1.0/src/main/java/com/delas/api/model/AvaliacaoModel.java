026c8efbf8875b50a27befe0087f922f10b935a1
Bárbara Galdino <122044386+babil0nia@users.noreply.github.com>
2024-12-04 15:14:13 -0300

    Update AvaliacaoModel.java

--- maisDelas1.0/src/main/java/com/delas/api/model/AvaliacaoModel.java
+++ maisDelas1.0/src/main/java/com/delas/api/model/AvaliacaoModel.java
@@ -1,12 +1,9 @@
 package com.delas.api.model;
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.Date;
 
 @Entity
 @Getter
@@ -14,6 +11,7 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "avaliacao")
+
 public class AvaliacaoModel {
 
     @Id
@@ -24,20 +22,6 @@ public class AvaliacaoModel {
     @JoinColumn(name = "idcontratacao", nullable = false)
     private ContratacaoModel idcontratacao;
 
-    @Column(name = "nota", nullable = false)
+    @Column(name = "nota")
     private int nota;
-
-    @Column(name = "comentario")
-    private String comentario;
-
-    @Column(name = "data")
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date data;
-
-    // Construtor específico para simplificar nos testes
-    public AvaliacaoModel(Long idavaliacao, ContratacaoModel idcontratacao, int nota) {
-        this.idavaliacao = idavaliacao;
-        this.idcontratacao = idcontratacao;
-        this.nota = nota;
-    }
 }
