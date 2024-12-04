47f8b9d0af103341d8778ecfc249b717f3be61ec
Bárbara Galdino <122044386+babil0nia@users.noreply.github.com>
2024-12-04 15:47:24 -0300

    Delete maisDelas1.0 directory
    deletando duplicata

--- maisDelas1.0/src/main/java/com/delas/api/repository/FavoritoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.delas.api.repository;
-import com.delas.api.model.FavoritoModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FavoritoRepository extends JpaRepository<FavoritoModel, Long>{
-}
--- maisDelas1.0/src/main/java/com/delas/api/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.delas.api.repository;
-import com.delas.api.model.UsuarioModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
-}
