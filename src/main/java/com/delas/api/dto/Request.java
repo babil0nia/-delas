91e3c1f6c93a16e2bc27c50eb339eb3b63b118ca
Bárbara Galdino <122044386+babil0nia@users.noreply.github.com>
2024-12-05 17:54:00 -0300

    Resquest.java
    adicionando request

---
+++ src/main/java/com/delas/api/dto/Request.java
@@ -0,0 +1,16 @@
+package com.delas.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+
+public class Request {
+    private String token;
+    private String novaSenha;
+
+}
