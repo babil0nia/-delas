0ee57e4f747ed30648b1fa596b9816a4708656e4
David Victor <davidvictorcontato7@gmail.com>
2024-12-05 11:21:16 -0300

    Update TokenRedefinicaoSenhaRepository.java

--- src/main/java/com/delas/api/repository/TokenRedefinicaoSenhaRepository.java
+++ src/main/java/com/delas/api/repository/TokenRedefinicaoSenhaRepository.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 public interface TokenRedefinicaoSenhaRepository extends JpaRepository<TokenRedefinicaoSenhaModel, Long> {
 
     Optional<TokenRedefinicaoSenhaModel> findByToken(String token);  // Buscar token por valor
-    Optional<TokenRedefinicaoSenhaModel> findByUsuarioId(Long usuarioId);  // Buscar token por ID do usuário
+//    Optional<TokenRedefinicaoSenhaModel> findByUsuarioId(Long Id);  // Buscar token por ID do usuário
 
     void deleteByToken(String token);
 }
