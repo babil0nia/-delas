5e1dc971053d37af63f3fdbaffd5c3789ca37a7c
David Victor <davidvictorcontato7@gmail.com>
2024-12-08 14:50:47 -0300

    Update SwaggerConfig.java

--- src/main/java/com/delas/api/config/SwaggerConfig.java
+++ src/main/java/com/delas/api/config/SwaggerConfig.java
@@ -1,8 +1,10 @@
 package com.delas.api.config;
 
-import io.swagger.v3.oas.models.info
-        .Info;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,10 +13,27 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        // Define o esquema de segurança JWT
+        SecurityScheme securityScheme = new SecurityScheme()
+                .name("BearerAuth")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        // Cria o componente com o esquema de segurança
+        Components components = new Components()
+                .addSecuritySchemes("BearerAuth", securityScheme);
+
+        // Define o requerimento de segurança global
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("BearerAuth");
+
         return new OpenAPI()
+                .components(components)
+                .addSecurityItem(securityRequirement)
                 .info(new Info()
                         .title("API Mais Delas")
                         .version("1.0.0")
-                        .description("Documentação da API Mais Delas"));
+                        .description("Documentação da API Mais Delas com suporte a autenticação JWT"));
     }
 }
