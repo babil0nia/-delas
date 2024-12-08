82855cbaa161a8a072ff6906dc33eb8094d48b7a
David Victor <davidvictorcontato7@gmail.com>
2024-12-08 15:03:39 -0300

    Update ServicosModel.java

--- src/main/java/com/delas/api/model/ServicosModel.java
+++ src/main/java/com/delas/api/model/ServicosModel.java
@@ -18,42 +18,44 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class ServicosModel {
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long idservicos; // Alteração no nome do campo ID
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idservicos; 
 
-        @Column(columnDefinition = "TEXT", nullable = false)
-        @NotBlank(message = "A descrição é obrigatória.")
-        private String descricao;
+    @Column(columnDefinition = "TEXT", nullable = false)
+    @NotBlank(message = "A descrição é obrigatória.")
+    private String descricao;
 
-        @Column(precision = 10, scale = 2, nullable = false)
-        @DecimalMin(value = "0.0", inclusive = false, message = "O preço deve ser maior que zero.") // Alteração na validação do preço
-        private BigDecimal preco;
+    @Column(precision = 10, scale = 2, nullable = false)
+    @DecimalMin(value = "0.0", inclusive = false, message = "O preço deve ser maior que zero.")
+    // Alteração na validação do preço
+    private BigDecimal preco;
 
-        @Column(length = 70, nullable = false)
-        @NotBlank(message = "O título é obrigatório.")
-        private String titulo;
+    @Column(length = 70, nullable = false)
+    @NotBlank(message = "O título é obrigatório.")
+    private String titulo;
 
-        @Column(name = "datacriacao_servico", nullable = false, updatable = false) // Alteração no nome da coluna
-        private LocalDateTime datacriacao;
+    @Column(name = "datacriacao_servico", nullable = false, updatable = false) 
+    private LocalDateTime datacriacao = LocalDateTime.now();
 
-        @Column(length = 100)
-        private String categoria;
+    @Column(length = 100, nullable = false)
+    private String categoria;
 
-        @Column(name = "idfavorito") // Alteração no nome da coluna
-        private Long idfavorito;
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private UsuarioModel usuario;
 
-        @Column(nullable = false)
-        @DecimalMin(value = "1.0", message = "A nota deve ser no mínimo 1.")
-        @DecimalMax(value = "5.0", message = "A nota deve ser no máximo 5.")
-        private Double nota;
 
-        // Método para definir o valor de datacriacao automaticamente
-        @PrePersist
-        protected void onCreate() {
-                // Verifica se a data não está definida para evitar sobrescrever um valor já existente
-                if (this.datacriacao == null) {
-                        this.datacriacao = LocalDateTime.now();
-                }
+    @Column
+    @DecimalMin(value = "1.0", message = "A nota deve ser no mínimo 1.")
+    @DecimalMax(value = "5.0", message = "A nota deve ser no máximo 5.")
+    private Double nota;
+
+    @PrePersist
+    protected void onCreate() {
+        // Verifica se a data não está definida para evitar sobrescrever um valor já existente
+        if (this.datacriacao == null) {
+            this.datacriacao = LocalDateTime.now();
         }
+    }
 }
