103e7e99e9d15e005641f205c640b73f2ee53767
David Victor <davidvictorcontato7@gmail.com>
2024-12-08 15:01:41 -0300

    Update FavoritoModel.java

--- src/main/java/com/delas/api/model/FavoritoModel.java
+++ src/main/java/com/delas/api/model/FavoritoModel.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 import java.time.LocalDateTime;
 
 @Table(name = "favorito")
@@ -15,23 +16,23 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class FavoritoModel {
 
-        @Id  // Define o campo como a chave primária da tabela
-        @GeneratedValue(strategy = GenerationType.IDENTITY)  // Define a estratégia de geração automática do valor da chave primária (auto-incremento)
-        private Long id;  // A chave primária, que é do tipo Long
+    @Id  // Define o campo como a chave primária da tabela
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-        @ManyToOne  // Define um relacionamento "muitos para um" entre FavoritoModel e ServicosModel (um serviço pode ser favoritado por muitos usuários)
-        @JoinColumn(name = "idservicofavorito", nullable = false)  // Define a coluna que representa a chave estrangeira (idservicofavorito) e especifica que não pode ser nula
-        private ServicosModel servicoFavorito;  // Relacionamento com a tabela "servicos", ou seja, um serviço que é favoritado
+    // Define a estratégia de geração automática do valor da chave primária (auto-incremento)
+    private Long idavaliacao;  // A chave primária, que é do tipo Long
 
-        @ManyToOne  // Define um relacionamento "muitos para um" entre FavoritoModel e UsuarioModel (um usuário pode favoritar muitos serviços)
-        @JoinColumn(name = "idprestadorfavorito", nullable = false)  // Define a coluna que representa a chave estrangeira (idprestadorfavorito) e especifica que não pode ser nula
-        private UsuarioModel usuarioFavorito;  // Relacionamento com a tabela "usuarios", ou seja, o usuário que favoritou o serviço
 
-        private LocalDateTime dataFavoritamento;  // Data e hora em que o serviço foi favoritado
+    @ManyToOne     // Define um relacionamento "muitos para um" entre FavoritoModel e ServicosModel (um serviço pode ser favoritado por muitos usuários)
+    @JoinColumn(name = "idservicofavorito", nullable = false)  // Define a coluna que representa a chave estrangeira (idservicofavorito) e especifica que não pode ser nula
+    private ServicosModel servicoFavorito;  // Relacionamento com a tabela "servicos", ou seja, um serviço que é favoritado
 
-        // Método para definir a dataFavoritamento automaticamente, que será executado antes de persistir a entidade no banco de dados
-        @PrePersist
-        protected void onCreate() {
-                this.dataFavoritamento = LocalDateTime.now();  // Define a data e hora atual como a data de favoritação
-        }
+    @ManyToOne
+    // Define um relacionamento "muitos para um" entre FavoritoModel e UsuarioModel (um usuário pode favoritar muitos serviços)
+    @JoinColumn(name = "idprestadorfavorito", nullable = false)
+    // Define a coluna que representa a chave estrangeira (idprestadorfavorito) e especifica que não pode ser nula
+    private UsuarioModel usuarioFavorito;  // Relacionamento com a tabela "usuarios", ou seja, o usuário que favoritou o serviço
+
+    private LocalDateTime dataFavoritamento = LocalDateTime.now();  // Data e hora em que o serviço foi favoritado
+
 }
